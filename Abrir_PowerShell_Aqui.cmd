91bf30afab7ea16215ed344b310ffddbfb3a5ae2
AmbiGEP <ambigep@local>
2025-09-19 00:09:41 +0100

    primeiro commit: PROCESSOS_GEP_Automacao

---
+++ Abrir_PowerShell_Aqui.cmd
@@ -0,0 +1,4 @@
+@echo off
+REM abre o Windows PowerShell na pasta desejada
+start "" "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoExit -NoLogo -Command ^
+  "Set-Location -LiteralPath 'H:\PROJECTOS\PROCESSOS_GEP_Automacao'"
